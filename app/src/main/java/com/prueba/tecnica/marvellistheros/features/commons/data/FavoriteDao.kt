c5aa4c0912ff12430285df151d3cd3a0ae128c2e
Jhon Casique <jhoncasique04@hotmail.com>
2021-03-17 20:53:19 +0100

    feat: add list layout for characters and character details also favorites characters

--- app/src/main/java/com/prueba/tecnica/marvellistheros/features/commons/data/FavoriteDao.kt
+++ app/src/main/java/com/prueba/tecnica/marvellistheros/features/commons/data/FavoriteDao.kt
@@ -1,7 +1,6 @@
 package com.prueba.tecnica.marvellistheros.features.commons.data
 
-import androidx.room.Dao
-import androidx.room.Query
+import androidx.room.*
 import com.prueba.tecnica.marvellistheros.features.commons.data.model.FavoriteDto
 
 @Dao
@@ -9,5 +8,14 @@ interface FavoriteDao {
 
     @Query("SELECT * FROM favorite")
     fun getAll(): List<FavoriteDto>
+
+    @Query("SELECT * FROM favorite WHERE favoriteId = :favoriteId")
+    fun get(favoriteId: Long): FavoriteDto
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun save(goalDataModel: FavoriteDto): Long
+
+    @Delete
+    fun delete(goalDataModel: FavoriteDto)
 
 }
